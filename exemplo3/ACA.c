1c672fcc4075b57277b635962a81732facb261e7
Fabricio Vilasboas <vilasboas@axondata.com.br>
2019-04-08 23:52:42 -0300

    Alteração: 1) Similaridade de cossenos; 2) pd > 90% e pp < 20%.

--- exemplo3/ACA.c
+++ exemplo3/ACA.c
@@ -16,9 +16,9 @@ int cell_has_item(const int x, const int y, Cell **B)
   return res;
 }
 
-int ant_has_item(Ant ant)
+int ant_has_item(Ant *ant)
 {
-  int res = (ant.item == NULL) ? 0 : 1;
+  int res = (ant->item == NULL) ? 0 : 1;
   return res;
 }
 
@@ -28,98 +28,77 @@ float cosine_similarity(float *item1, float *item2, const int n)
   float dot = 0.0, denom_a = 0.0, denom_b = 0.0 ;
   for(i = 0; i < n; ++i)
   {
-    dot += item1[i] * item2[i] ;
-    denom_a += item1[i] * item1[i] ;
-    denom_b += item2[i] * item2[i] ;
+    dot += item1[i] * item2[i];
+    denom_a += item1[i] * item1[i];
+    denom_b += item2[i] * item2[i];
   }
-  return dot / (sqrt(denom_a) * sqrt(denom_b));
-}
-
-float dist(float *item1, float *item2, const int n)
-{
-  int i;
-  float sum = 0.0;
-  //fprintf(stdout, "\tdist:1; n: %d.\n", n);
-  for (i = 0; i < n; i++)
-  {
-    //fprintf(stdout, "\t\n");
-    sum += item1[i]*item1[i] - item2[i]*item2[i];
-  }
-  //fprintf(stdout, "\tdist:3\n");
-  return sqrt(sum);
+  return fabsf(dot / (sqrtf(denom_a) * sqrtf(denom_b)));
 }
 
 float f(Cell **B, float *item, const int x, const int y, const int nb, const int m, const int s_items)
 {
   int i, j;
-  float sum = 0;
-  //fprintf(stdout, "\tf:1 -> x: %d, y: %d.\n", x, y);
+  float sum = 0.0;
   for (i = x - nb; i < x + nb; i++)
     for (j = y - nb; j < y + nb; j++)
     {
-      //fprintf(stdout, "\tf:2 -> x: %d, y: %d.\n", i, j);
       if (i >= 0 && i < m && j >= 0 && j < m)
       {
         if (cell_has_item(i, j, B) == 1)
         {
-          //fprintf(stdout, "\tf:3\n");
-          //sum += 1-dist(item, B[i][j].item, s_items);
           sum += cosine_similarity(item, B[i][j].item, s_items);
         }
       }
     }
-  //fprintf(stdout, "\tf:4\n");
-  return abs(sum/(nb*nb));
+  return fabsf(sum/(2*(nb*nb)));
 }
 
-void ant_dynamic(Ant *ant, Cell **B, const float k1, const float k2, const int m,
-                const int nb, const int s_items)
+void ant_dynamic(Ant *ant, Cell **B, const int m, const int nb, const int s_items)
 {
   float pp, pd, fx;
   size_t size = sizeof(float)*s_items;
 
   //Probability of the ant drop the item
-  if (ant_has_item(*ant) == 1 && cell_has_item(ant->x, ant->y, B) == 0)
+  if (ant_has_item(ant) == 1 && cell_has_item(ant->x, ant->y, B) == 0)
   {
     pd = f(B, ant->item, ant->x, ant->y, nb, m, s_items);
-    //pd = (fx < k2) ? 2*fx : 1;
-    fprintf(stdout, "\tDrop; pd: %.2f\n", pd);
-    if (pd >= 0.2)
+    //fprintf(stdout, "\tDrop; pd: %f\n", pd);
+    if (pd >= 0.9)
     {
       B[ant->x][ant->y].item = (float*)malloc(size);
       memcpy(B[ant->x][ant->y].item, ant->item, size);
+      free(ant->item);
       ant->item = NULL;
       B[ant->x][ant->y].item_id = ant->item_id;
       ant->item_id = -1;
-      fprintf(stdout, "\tDroped item %d on cell [%d, %d] with pd = %.2f.\n", B[ant->x][ant->y].item_id, ant->x, ant->y, pd);
+      //fprintf(stdout, "\tDroped item %d on cell [%d, %d] with pd = %f.\n", B[ant->x][ant->y].item_id, ant->x, ant->y, pd);
     }
     return;
   }
+
   // Probability of the ant pick the item
-  if (ant_has_item(*ant) == 0 && cell_has_item(ant->x, ant->y, B) == 1)
+  if (ant_has_item(ant) == 0 && cell_has_item(ant->x, ant->y, B) == 1)
   {
-    //pp = pow( k1 / (k1 + f(B, B[ant->x][ant->y].item, ant->x, ant->y, nb, m, s_items) ), 2);
     pp = f(B, B[ant->x][ant->y].item, ant->x, ant->y, nb, m, s_items);
-    fprintf(stdout, "\tPick; pp: %.2f.\n", pp);
-    if (pp < 0.2)
+    //fprintf(stdout, "\tPick; pp: %f\n", pp);
+    if (pp <= 0.2)
     {
       ant->item = (float*)malloc(size);
       memcpy(ant->item, B[ant->x][ant->y].item, size);
+      free(B[ant->x][ant->y].item);
       B[ant->x][ant->y].item = NULL;
       ant->item_id = B[ant->x][ant->y].item_id;
       B[ant->x][ant->y].item_id = -1;
-      fprintf(stdout, "\tPicked item %d on cell [%d, %d] with pp = %.2f.\n", ant->item_id, ant->x, ant->y, pp);
+      //fprintf(stdout, "\tPicked item %d on cell [%d, %d] with pp = %f.\n", ant->item_id, ant->x, ant->y, pp);
     }
     return;
   }
 }
 
-void move_ant(Ant *ant, Cell **B, const int m, const int nb, const float k1,
-              const float k2, const int s_items)
+void move_ant(Ant *ant, Cell **B, const int m, const int nb, const int s_items)
 {
   int direction, moved = 0, stuck = 0;
 
-  //fprintf(stdout, "\tx: %d, y: %d\n", ant->x, ant->y);
   do
   {
     direction = rand()%4;
@@ -132,11 +111,10 @@ void move_ant(Ant *ant, Cell **B, const int m, const int nb, const float k1,
         {
           if (B[ant->x-1][ant->y].has_ant == 0)
           {
-            fprintf(stdout, "\tMove up\n");
             B[ant->x][ant->y].has_ant = 0;
             ant->x -= 1;
             B[ant->x][ant->y].has_ant = 1;
-            ant_dynamic(ant, B, k1, k2, m, nb, s_items);
+            ant_dynamic(ant, B, m, nb, s_items);
             moved = 1;
           }
         }
@@ -147,11 +125,10 @@ void move_ant(Ant *ant, Cell **B, const int m, const int nb, const float k1,
         {
           if (B[ant->x+1][ant->y].has_ant == 0)
           {
-            fprintf(stdout, "\tMove down\n");
             B[ant->x][ant->y].has_ant = 0;
             ant->x += 1;
             B[ant->x][ant->y].has_ant = 1;
-            ant_dynamic(ant, B, k1, k2, m, nb, s_items);
+            ant_dynamic(ant, B, m, nb, s_items);
             moved = 1;
           }
         }
@@ -162,11 +139,10 @@ void move_ant(Ant *ant, Cell **B, const int m, const int nb, const float k1,
         {
           if (B[ant->x][ant->y+1].has_ant == 0)
           {
-            fprintf(stdout, "\tMove rigth\n");
             B[ant->x][ant->y].has_ant = 0;
             ant->y += 1;
             B[ant->x][ant->y].has_ant = 1;
-            ant_dynamic(ant, B, k1, k2, m, nb, s_items);
+            ant_dynamic(ant, B, m, nb, s_items);
             moved = 1;
           }
         }
@@ -177,33 +153,29 @@ void move_ant(Ant *ant, Cell **B, const int m, const int nb, const float k1,
         {
           if (B[ant->x][ant->y-1].has_ant == 0)
           {
-            fprintf(stdout, "\tMove left\n");
             B[ant->x][ant->y].has_ant = 0;
             ant->y -= 1;
             B[ant->x][ant->y].has_ant = 1;
-            ant_dynamic(ant, B, k1, k2, m, nb, s_items);
+            ant_dynamic(ant, B, m, nb, s_items);
             moved = 1;
           }
         }
       break;
     }
-    fprintf(stdout, "\tx: %d, y: %d -> stuck: %d.\n", ant->x, ant->y, stuck);
   } while(moved == 0 && stuck <= 4);
 }
 
 void simulte(Ant *ants, Cell **B, const int m, const int n_ants, const int nb,
-            const int s_items, const int max_it, const float k1, const float k2)
+            const int s_items, const int max_it)
 {
-  int it, i;
+  int it, ant;
 
   for (it = 0; it < max_it; it++)
   {
-    fprintf(stdout, "\nIteration: %d.\n", it);
-    for (i = 0; i < n_ants; i++)
+    for (ant = 0; ant < n_ants; ant++)
     {
-      //fprintf(stdout, "%d,%d,%d,%d,%d\n", it, i, ants[i].x, ants[i].y, ants[i].item_id);
-      fprintf(stdout, "Ant: %d\n", i);
-      move_ant(&ants[i], B, m, nb, k1, k2, s_items);
+      move_ant(&ants[ant], B, m, nb, s_items);
+      //fprintf(stdout, "%d,%d,%d,%d,%d\n", it, ant, ants[ant].x, ants[ant].y, ants[ant].item_id);
     }
   }
 }
@@ -239,7 +211,6 @@ float** items_allocation(const int n_items, const int s_items)
     for (j = 0; j < s_items; j++)
     {
       items[i][j] = rand()%1000;
-      //fprintf(stdout, "tems_allocation; item[%d][%d]: %.2f.\n", i, j, items[i][j]);
     }
 
   return items;
@@ -263,10 +234,10 @@ void initialize(const int m, const int n_ants, const int n_items, const int s_it
       B[x][y].item = (float*)malloc(size);
       memcpy(B[x][y].item, items[i], size);
       B[x][y].item_id = i;
-      //fprintf( stdout, "i: %d, x: %d, y: %d, has_ant: %d, has_item: %d, item_id: %d.\n", i, x, y, B[x][y].has_ant, cell_has_item(x, y, B), B[x][y].item_id);
       i++;
     }
   } while(i != n_items);
+  free(items);
 
   // Initialize Cell with ants
   i = 0;
@@ -281,7 +252,6 @@ void initialize(const int m, const int n_ants, const int n_items, const int s_it
       ants[i].item = NULL;
       ants[i].item_id = -1;
       B[x][y].has_ant = 1;
-      //fprintf( stdout, "i: %d, x: %d, y: %d, has_ant: %d, has_item: %d.\n", i, x, y, B[x][y].has_ant, cell_has_item(x, y, B));
       i++;
     }
   } while(i != n_ants);
@@ -293,7 +263,8 @@ void grid_print(Cell **B, const int m)
   int i, j;
   for (i = 0; i < m; i++)
     for (j = 0; j < m; j++)
-      fprintf(stdout, "[%d, %d]: %d\n", i,j,B[i][j].item_id);
+      //fprintf(stdout, "%d,%d,%d\n", i, j, cell_has_item(i, j, B));
+      fprintf(stdout, "%d\n", cell_has_item(i, j, B));
 }
 
 int main (int argc, char **argv)
@@ -306,6 +277,7 @@ int main (int argc, char **argv)
     nb: number of neighbors;
     k1: threshold constant defined by the user;
     k2: threshold constant defined by the user;
+    a: scale dimensionality factor
     f: perceived fraction of items;
     B: grid of cells;
     ants: vector of ants;
@@ -314,19 +286,20 @@ int main (int argc, char **argv)
     s_items: number of elementes per item.
   */
 
-  int m = 100, n_ants = 1000, max_it = 1000, nb = 10, n_items = 5000, s_items = 1000;
-  float k1 = 1, k2 = 1  , **items;
+  // A razão de proporcionalidade de m -> n_ants e n_items é de uma ordem de grandeza.
+  int m = 120, n_ants = 300, max_it = 100000000, nb = 10, n_items = 500, s_items = 100;
+  float **items;
   Cell **B;
   Ant *ants = (Ant*)malloc(sizeof(Ant) * n_ants);
   srand(time(NULL));
-  fprintf(stdout, "Allocating B\n");
+  //fprintf(stdout, "Allocating B\n");
   B = grid_allocation(m);
-  fprintf(stdout, "Allocating items\n");
+  //fprintf(stdout, "Allocating items\n");
   items = items_allocation(n_items, s_items);
-  fprintf(stdout, "Initialize\n");
+  //fprintf(stdout, "Initialize\n");
   initialize(m, n_ants, n_items, s_items, B, ants, items);
-  fprintf(stdout, "Simulate\n");
-  simulte(ants, B, m, n_ants, nb, s_items, max_it, k1, k2);
+  //fprintf(stdout, "Simulate\n");
+  simulte(ants, B, m, n_ants, nb, s_items, max_it);
   grid_print(B, m);
 
   return 0;
