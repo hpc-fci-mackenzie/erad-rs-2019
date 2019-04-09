01f4509d7672c65a48829b364ac747952bfd5edb
Fabricio Vilasboas <vilasboas@axondata.com.br>
2019-04-08 23:56:07 -0300

    Adicionado script para plot da matriz de agrupamento.

---
+++ exemplo3/plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+n = 120
+
+csv = np.genfromtxt ('ACA.out', delimiter="")
+
+aa = np.zeros((n,n))
+
+for i in range(n):
+  for j in range(n):
+    aa[i,j] = csv[i*n + j]
+
+plt.matshow(aa)
+plt.show()
